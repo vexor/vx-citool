f14a0fc042db81333534e4f41e29b74f4f9a8069
Dmitry Galinsky <dima.exe@gmail.com>
2015-03-19 00:43:23 +0300

    fix chdir with non existing directory

--- lib/vx/citool/actions/chdir.rb
+++ lib/vx/citool/actions/chdir.rb
@@ -6,9 +6,12 @@ module Vx
         log_command "cd #{args}"
         dest = normalize_env_value(args)
         dest = File.expand_path(dest)
-        Dir.chdir(dest)
-
-        Succ.new(0,  "The command 'cd #{args}' exited with code 0")
+        begin
+          Dir.chdir(dest)
+          Succ.new(0,  "The command 'cd #{args}' exited with code 0")
+        rescue Errno::ENOENT => e
+          Fail.new(1,  "The command 'cd #{args}' failed, #{e.message}")
+        end
       end
     end
 
