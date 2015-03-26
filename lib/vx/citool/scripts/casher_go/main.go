535e6315188aa45859bb6a53383a4bc58cb294ef
Pavel Argentov <argentoff@gmail.com>
2015-03-26 16:30:43 +0300

    All 'add' functionality except 'tar'

--- lib/vx/citool/scripts/casher_go/main.go
+++ lib/vx/citool/scripts/casher_go/main.go
@@ -4,6 +4,9 @@ import (
 	"code.google.com/p/gcfg"
 	"fmt"
 	"github.com/codegangsta/cli"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"time"
@@ -31,23 +34,34 @@ var (
 	mtimes Mtimes
 )
 
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func tar(flag string, tarFileName string, paths ...string) (err error) {
+	// TODO: implement
+	return
+}
+
 func (cfg *Config) build(c *cli.Context) {
 	err := gcfg.ReadFileInto(cfg, c.String("config"))
-	if err != nil {
-		fmt.Println("Bad config:", err)
-		os.Exit(1)
-	}
-	cfg.CasherDir = c.String("casher_dir")
-	cfg.MtimeFile = fmt.Sprintf("%s/%s", cfg.CasherDir, cfg.Files.MtimeFile)
-	cfg.Md5File = fmt.Sprintf("%s/%s", cfg.CasherDir, cfg.Files.Md5File)
-	cfg.FetchTar = fmt.Sprintf("%s/%s", cfg.CasherDir, cfg.Files.FetchTar)
-	cfg.PushTar = fmt.Sprintf("%s/%s", cfg.CasherDir, cfg.Files.PushTar)
+	check(err)
+
+	cfg.CasherDir, err = filepath.Abs(c.String("casher_dir"))
+	// TODO: решить на filepath целиком
+	cfg.MtimeFile, err = filepath.Abs(fmt.Sprintf("%s/%s", cfg.CasherDir, cfg.Files.MtimeFile))
+	cfg.Md5File, err = filepath.Abs(fmt.Sprintf("%s/%s", cfg.CasherDir, cfg.Files.Md5File))
+	cfg.FetchTar, err = filepath.Abs(fmt.Sprintf("%s/%s", cfg.CasherDir, cfg.Files.FetchTar))
+	cfg.PushTar, err = filepath.Abs(fmt.Sprintf("%s/%s", cfg.CasherDir, cfg.Files.PushTar))
+	check(err)
 }
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "casher"
-	app.Usage = "TODO: WRITEME"
+	app.Usage = "Caches the specified files"
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
@@ -65,13 +79,16 @@ func main() {
 	}
 
 	app.Before = func(c *cli.Context) (err error) {
+		// TODO: extract function
 		cfg.build(c)
 		err = os.MkdirAll(cfg.CasherDir, 0755)
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		mtimes = make(Mtimes)
+		check(err)
+
+		yml, err := ioutil.ReadFile(cfg.MtimeFile)
+		check(err)
+
+		err = yaml.Unmarshal(yml, &mtimes)
+		check(err)
 		return
 	}
 
@@ -81,16 +98,28 @@ func main() {
 			Aliases: []string{"a"},
 			Usage:   "adds paths to cache",
 			Action: func(c *cli.Context) {
+				// TODO: extract function
+				log.Println("adding: started")
+
 				paths := c.Args()
+
 				for _, path := range paths {
 					path, _ = filepath.Abs(path)
-					fmt.Printf("adding %s to cache\n", path)
+					log.Printf("adding %s to cache\n", path)
 					os.MkdirAll(path, 0755)
-					// tar goes here
+
+					err := tar("x", cfg.FetchTar, path)
+					if err != nil {
+						log.Println(path, "is not yet cached")
+					}
+
 					mtimes[path] = time.Now().Unix()
 				}
-				fmt.Println(mtimes)
-				// mtimes marshlling to yaml goes here
+				yml, _ := yaml.Marshal(mtimes)
+
+				err := ioutil.WriteFile(cfg.MtimeFile, yml, 0644)
+				check(err)
+				log.Println("adding: finished")
 			},
 		},
 		{
