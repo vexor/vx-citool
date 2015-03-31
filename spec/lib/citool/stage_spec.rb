3daee07228e6d73e9cdeb0f291e4a6237baf0ca1
Theodore Konukhov <komolov.f@gmail.com>
2015-03-31 23:06:26 +0300

    stop if env var is bad

--- spec/lib/citool/stage_spec.rb
+++ spec/lib/citool/stage_spec.rb
@@ -14,18 +14,30 @@ describe Vx::Citool::Stage do
   }
 
   context "environment" do
-    before { subject }
 
     it "expands env" do
+      expect(subject.code).to eq 0
       expect(ENV['TEST_EXPAND']).to match(/#{ENV['USER']}\z/)
     end
 
     it "adds secret env" do
+      expect(subject.code).to eq 0
       expect(ENV['TEST_SECRET']).to eq "secret"
     end
 
     it "adds env" do
+      expect(subject.code).to eq 0
       expect(ENV['TEST_ENV']).to eq "test_env"
+    end
+  end
+
+  context "failing environment" do
+    let(:environment) do
+      { "BAD_VAR" => "bad var (is here)" }
+    end
+
+    it "fails" do
+      expect(subject.code).not_to eq 0
     end
   end
 end
