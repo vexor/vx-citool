0da7a1fd29cfd5276901822b8ecdf64af5b19898
Alexander Kirillov <saratovsource@gmail.com>
2018-01-23 20:38:09 +0400

    Update stage.rb

--- lib/vx/citool/stage.rb
+++ lib/vx/citool/stage.rb
@@ -84,8 +84,9 @@ module Vx
       private
 
       def add_env(name, value)
-        return if value.size > 1096
-        Env.export!(name, value)
+        if value.size > 1096 do
+          Env.export!(name, value)
+        end
       end
 
     end
